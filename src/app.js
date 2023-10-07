7b94f6776bdb5788abbf806aa0e09e5478c2c082
Ann-tech <Onyekaann17@gmail.com>
2023-10-07 15:12:34 +0100

    add error handler middleware

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+const errorHandlerMiddleware = require('./middlewares/errorHandler.middleware')
+
 if (process.env.NODE_ENV !== "test") {
     app.listen(PORT, () => {
         console.log(`Server is running on PORT ${PORT}`);
     });
 }
 
+app.use( errorHandlerMiddleware );
