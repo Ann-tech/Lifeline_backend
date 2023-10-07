4ffb3b838fbd52d129dc60b53da971a2577a2328
Ann-tech <Onyekaann17@gmail.com>
2023-10-07 15:17:16 +0100

    add logging functionality

--- src/app.js
+++ src/app.js
@@ -3,11 +3,28 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const errorHandlerMiddleware = require('./middlewares/errorHandler.middleware')
+const errorHandlerMiddleware = require('./middlewares/errorHandler.middleware');
+const morganMiddleware = require('./middlewares/morgan.middleware');
+
+const logger = require('./logging/logger');
+
+app.use( helmet() );
+
+require('dotenv').config();
+
+// Add the morgan middleware
+app.use(morganMiddleware);
+
+//To parse url encoded data
+app.use(express.urlencoded( {extended: false} ));
+
+//To parse data passed via body
+app.use(express.json());
+
 
 if (process.env.NODE_ENV !== "test") {
     app.listen(PORT, () => {
-        console.log(`Server is running on PORT ${PORT}`);
+        logger.info(`Server is running on PORT ${PORT}`);
     });
 }
 
