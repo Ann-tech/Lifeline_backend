50a7c4f19335ab7b7b92c4ea26955b8011e08830
ann-tech <Onyekaann17@gmail.com>
2023-10-26 01:06:14 +0100

    enable cors on socket

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
 
 
 const PORT = process.env.PORT || 8080;
